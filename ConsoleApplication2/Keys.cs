13c4d99a36107ebb20cca4b7c12819e3db4a0b4e
Bảo Duy <duybao05123@gmail.com>
2018-03-10 11:45:08 +0700

    All Code

---
+++ ConsoleApplication2/Keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KeyLogger
+{
+    internal class Keys
+    {
+        public static Keys K { get; internal set; }
+        public static Keys LControlKey { get; internal set; }
+        public static Keys RControlKey { get; internal set; }
+        public static Keys Separator { get; internal set; }
+
+        public static explicit operator Keys(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
